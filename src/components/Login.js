e82b090d5949dd15bdc5604b1a6c82ab60ba7d2c
Safak Yildiz <shafakyildiz@gmail.com>
2021-10-26 23:27:39 +0300

    ed.t

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,8 @@
 import React from "react";
-// import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-// import Home from "./Home";
-// import About from "./About";
-// import Expenses from "./Expenses";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Button from "@mui/material/Button";
 import LoginIcon from "@mui/icons-material/Login";
+import Balance from "./Balance";
 
 function Login() {
   return (
@@ -18,27 +16,20 @@ function Login() {
         <input label="password" type="password" />
       </div>
       <div className="loginBtn">
-        <Button variant="contained" endIcon={<LoginIcon />}>
-          Login
-        </Button>
+        <Router>
+          <Link to="/balance">
+            <Button variant="contained" endIcon={<LoginIcon />}>
+              Login
+            </Button>
+          </Link>
+
+          <Switch>
+            <Route exact path="/balance">
+              <Balance />
+            </Route>
+          </Switch>
+        </Router>
       </div>
-      {/* <Router>
-        <Link to="/home">
-          <button>Login</button>
-          <img src="" alt="" />
-        </Link>
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/expenses">
-            <Expenses />
-          </Route>
-          <Route path="/home">
-            <Home />
-          </Route>
-        </Switch>
-      </Router> */}
     </div>
   );
 }
