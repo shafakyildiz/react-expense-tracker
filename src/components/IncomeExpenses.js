9dbdcbdef3223f35345b7ea5d53b49a3600a5b2a
Safak Yildiz <shafakyildiz@gmail.com>
2021-10-27 11:45:30 +0300

    printed income and outcome to the screen

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -10,20 +10,19 @@ function IncomeExpenses() {
     .reduce((acc, item) => (acc += item), 0)
     .toFixed(2);
 
-  const outcome = amounts
-    .filter((item) => item > 0)
-    .reduce((acc, item) => (acc += item), 0)
-    .toFixed(2);
+  const outcome =
+    amounts.filter((item) => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -(1).toFixed(2);
 
   return (
     <div className="inc-exp-container">
       <div>
         <h4>Income</h4>
-        <p className="money plus">+$0.00</p>
+        <p className="money plus">+${income}</p>
       </div>
       <div>
         <h4>Expense</h4>
-        <p className="money minus">-$0.00</p>
+        <p className="money minus">-${outcome}</p>
       </div>
     </div>
   );
