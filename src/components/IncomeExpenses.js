b3cfe6ccdcf1f22cd6bbcbc58e289fed582842b7
Safak Yildiz <shafakyildiz@gmail.com>
2021-10-27 11:43:58 +0300

    added income and outcome variables

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -5,6 +5,16 @@ function IncomeExpenses() {
   const { transactions } = useContext(GlobalContext);
   const amounts = transactions.map((transaction) => transaction.amount);
 
+  const income = amounts
+    .filter((item) => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
+  const outcome = amounts
+    .filter((item) => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
   return (
     <div className="inc-exp-container">
       <div>
