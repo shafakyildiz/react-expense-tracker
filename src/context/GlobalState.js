cd2cfadcb347cdaa13c78c92984cedc53a1471e6
Safak Yildiz <shafakyildiz@gmail.com>
2021-10-27 11:48:54 +0300

    added delete functionality to transactions

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -18,6 +18,11 @@ export const GlobalContext = createContext(initialState);
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
 
+  // Actions
+  function deleteTransaction(id) {
+    dispatch({ type: "DELETE_TRANSACTION", payload: id });
+  }
+
   return (
     <GlobalContext.Provider value={{ transactions: state.transactions }}>
       {children}
